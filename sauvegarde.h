5a055fe23ab14d78ee64d89eafd6b16481528e15
Warzaros <62097319+Miner-code@users.noreply.github.com>
2022-06-17 01:34:22 +0200

    Sauvegarde.h

---
+++ sauvegarde.h
@@ -0,0 +1,19 @@
+int main(void);
+{
+    FILE *fic = fopen("sauvegarde.c", "r+");
+    int iteration = 0;
+    
+    if (fic == NULL)
+      exit(1);
+}
+
+int fic;
+
+for (fic = 0 ; fic <100000 ; fic++)
+{
+  scanf(fic, %d, &iteration);
+  printf("Sauvegarde : %d\n", iteration);
+}
+
+return 0;
+}
